3d6df250131fa44d25e6d969138720eea58fc5e8
Aymeric DE LANGE <aymericdelange80@gmail.com>
2024-05-05 17:33:54 +0200

    feat: product page

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -7,13 +7,13 @@
                 <div class="product-info">
                     <div class="product-images">
                         <div class="images">
-                            <img src="" alt="">
-                            <img src="" alt="">
-                            <img src="" alt="">
-                            <img src="" alt="">
+                            <img src="/products/product-27-1 (4) 1.jpg" alt="">
+                            <img src="/products/product-27-2 1.jpg" alt="">
+                            <img src="/products/product-27-3 1.jpg" alt="">
+                            <img src="/products/product-27-4 1.jpg" alt="">
                         </div>
                         <div class="image-showing">
-                            <img src="" alt="">
+                            <img src="/products/image.jpg" alt="" style="width: 640px;">
                             <div class="tag"></div>
                             <div class="cart-btn"></div>
                         </div>
@@ -22,34 +22,112 @@
                     <div class="product-details">
                         <div class="description">
                             <h2>Black Automatic Watch</h2>
-                            <p>The St. Louis Meramec Canoe Company was founded by Alfred Wickett in 1922. Wickett had
-previously worked for the Old Town Canoe Co from 1900 to 1914. Manufacturing of the classic
-wooden canoes in Valley Park, Missouri ceased in 1978.</p>
+                            <p>
+                                The St. Louis Meramec Canoe Company was founded by Alfred Wickett in 1922. Wickett had
+                                previously worked for the Old Town Canoe Co from 1900 to 1914. Manufacturing of the classic
+                                wooden canoes in Valley Park, Missouri ceased in 1978.
+                            </p>
                         </div>
                         <div class="fabric">
-                            <h2>Fabric + Care</h2>
-                            <p>Material: Soft wool blend</p>
-                            <p>Color: Various</p>
+                            <h3>Fabric + Care</h3>
+                            <div class="fabric-details">
+                                <p>Material: Soft wool blend</p>
+                                <p>Color: Various</p>
+                            </div>
                         </div>
-                        <div class="sales"></div>
-                        <div class="keywords"></div>
+                        <div class="sales">
+                            <h3>Sale performance</h3>
+                            <div class="sale-details">
+                                <p>Sales: 0</p>
+                                <p>Review Count: -</p>
+                                <p>Review Average: -</p>
+                            </div>
+                        </div>
+                        <div class="keywords">
+                            <h3>Keywords</h3>
+                            <div class="keywords-items">
+                                <!-- SVG / 12px regular --body-text-color | * 4 -->
+                                <div class="keyword-item">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/><path d="M20 3v4"/><path d="M22 5h-4"/><path d="M4 17v2"/><path d="M5 18H3"/></svg>
+                                    <p>men's fashion</p>
+                                </div>
+                                <div class="keyword-item">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/><path d="M20 3v4"/><path d="M22 5h-4"/><path d="M4 17v2"/><path d="M5 18H3"/></svg>
+                                    <p>winter hat</p>
+                                </div>
+                                <div class="keyword-item">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/><path d="M20 3v4"/><path d="M22 5h-4"/><path d="M4 17v2"/><path d="M5 18H3"/></svg>
+                                    <p>colorful accessory</p>
+                                </div>
+                                <div class="keyword-item">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"/><path d="M20 3v4"/><path d="M22 5h-4"/><path d="M4 17v2"/><path d="M5 18H3"/></svg>
+                                    <p>warm headwear</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="add-to-cart">
                     <div class="sticky-container">
                         <div class="sticky">
-                            <div class="price-container"></div>
-                            <div class="size-container"></div>
-                            <div class="quantity"></div>
-                            <div class="total-price-info"></div>
+                            <div class="price-container" style="display: flex; justify-content: space-between; align-items: flex-start;">
+                                <div class="reviews" style="display: flex; align-items: center; gap: 6px; font-size: 16px; font-weight: 600; line-height: 24px;">
+                                    <img src="/icons/star.svg" alt="">
+                                    <p>4.9</p>
+                                    <p>.</p>
+                                    <p style="text-decoration: underline;">142 reviews</p>
+                                </div>
+                                <div class="price-lg" style="text-align: end;">
+                                    <h3>$169.99</h3>
+                                    <p style="font-size: 14px; line-height: 20px; font-weight: 500; text-decoration: line-through;">$199.99</p>
+                                </div>
+                            </div>
+                            <div class="size-container">
+                                <h5>Size: S</h5>
+                                <div class="sizes">
+                                    <div class="size-item">S</div>
+                                    <div class="size-item">M</div>
+                                    <div class="size-item">L</div>
+                                    <div class="size-item">XL</div>
+                                    <div class="size-item">2XL</div>
+                                </div>
+                            </div>
+                            <div class="quantity">
+                                <img src="/quantity.png" alt="">
+                                <!-- A FAIRE REELLEMENT !!! -->
+                                <div class="quantity-input"></div>
+                                <div class="cart-button"></div>
+                            </div>
+                            <div class="total-price-info">
+                                <div class="subtotal">
+                                    <div class="subtotal-price">
+                                        <p>$169.99 x 1</p>
+                                        <p>$169.99</p>
+                                    </div>
+                                    <div class="tax">
+                                        <p>Tax estimate</p>
+                                        <p>$0</p>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="total">
+                                    <h5>Total</h5>
+                                    <h5>$169.99</h5>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="recommendation-section">
                 <div class="recommendations">
-                    <h2></h2>
-                    <div></div>
+                    <h2>Recommended products</h2>
+                    <div style="display: flex; gap: 20px;">
+                        <ProductCard/>
+                        <ProductCard/>
+                        <ProductCard/>
+                        <ProductCard/>
+                    </div>
                 </div>
             </div>
         </div>
@@ -59,7 +137,6 @@ wooden canoes in Valley Park, Missouri ceased in 1978.</p>
 <style scoped lang="scss">
 main {
     max-width: 1536px;
-    width: 100%;
     margin: auto;
     padding-inline: 120px;
 
@@ -79,6 +156,7 @@ main {
 .product-content {
     display: flex;
     gap: 32px;
+    width: 100%;
 }
 
 .product-info {
@@ -103,8 +181,162 @@ main {
 
 
 .add-to-cart {
-    width: 460px;
-    height: 105px;
-    background: #ccc;
+    // min-width: 460px;
+    // width: 460px;
+
+    max-width: 460px;
+    width: 100%;
+    // background: #ccc;
+
+    .sticky-container {
+        border: 1px solid var(--border-color);
+        border-radius: 16px;
+
+        width: 460px;
+        height: auto;
+
+        .sticky {
+            display: flex;
+            flex-direction: column;
+            gap: 32px;
+            padding: 33px;
+        }
+    }
+}
+
+.description, .fabric, .sales {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+}
+
+.product-images {
+    display: flex;
+    gap: 24px;
+    padding-bottom: 12px;
+
+    img {
+        border-radius: 16px;
+    }
+
+    .images {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+    }
+
+    .image-showing {
+        max-width: 640px;
+        max-height: 678px;
+    }
+}
+
+.keywords {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    .keywords-items {
+        display: flex;
+        gap: 8px;
+
+        .keyword-item {
+            display: flex;
+            align-items: center;
+            gap: 4px;
+
+
+            border: 1px solid var(--border-color);
+            border-radius: 9999px;
+
+            padding-block: 8px;
+            padding-inline: 14px;
+
+            //
+            p {
+                font-size: 12px;
+                font-weight: 400; 
+                line-height: 16px; 
+                color: var(--body-text-color);
+            }
+
+
+            //
+
+            svg {
+                width: 12px;
+                height: 12px
+            }
+        }
+    }
+}
+
+.recommendation-section {
+
+    .recommendations {
+        display: flex;
+        flex-direction: column;
+        gap: 40px;
+    }
+}
+
+
+.total-price-info {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+    
+    hr {
+        width: 100%;
+    }
+}
+
+.subtotal {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+}
+
+.subtotal-price, .tax, .total {
+    display: flex;
+    justify-content: space-between;
+}
+
+.size-container {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+
+    .sizes {
+        display: flex;
+        gap: 12px;
+        // flex-wrap: wrap;
+
+        .size-item {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            width: 72px;
+            padding-block: 10px;
+
+            border: 1px solid var(--border-color);
+            border-radius: 12px;
+
+            &:first-child {
+                background: var(--vibrant-color);
+                color: #FFF;
+            }
+
+            &:last-child {
+                color: var(--border-color);
+            }
+        }
+    }
 }
 </style>
